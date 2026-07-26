a02c5b351c0b892de416427962d9f3503ce4fe08
hashimoto-kei <hashimoto.keigo88@gmail.com>
2026-07-26 18:00:03 +0900

    Implement memo application

---
+++ main.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require_relative 'models/memo'
+
+get '/' do
+  erb :index, :locals => {title: 'Top', memos: Memo.all}
+end
+
+get '/memos/new' do
+  erb :new, :locals => {title: 'New memo'}
+end
+
+post '/memos' do
+  memo = Memo.new(params['title'], params['detail'])
+  memo.save
+  redirect to('/')
+end
+
+get '/memos/:id' do |id|
+  erb :show, :locals => {title: 'Show memo', memo: Memo.find(id)}
+end
+
+get '/memos/:id/edit' do |id|
+  erb :edit, :locals => {title: 'Edit memo', memo: Memo.find(id)}
+end
+
+put '/memos/:id' do |id|
+  memo = Memo.find(id)
+  memo.title = params['title']
+  memo.detail = params['detail']
+  memo.save
+  redirect to('/')
+end
+
+delete '/memos/:id' do |id|
+  memo = Memo.find(id)
+  memo.destroy
+  redirect to('/')
+end
