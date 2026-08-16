d6e0c4cf94a9b655a5e4a9f98ce07dfbc85c6a4e
hashimoto-kei <hashimoto.keigo88@gmail.com>
2026-08-17 02:14:29 +0900

    Implement memo application

---
+++ main.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require_relative 'models/memo'
+
+get '/' do
+  redirect to('/memos')
+end
+
+get '/memos' do
+  @title = 'Top'
+  @memos = Memo.all
+  erb :index
+end
+
+get '/memos/new' do
+  @title = 'New memo'
+  @memo = Memo.new
+  erb :new
+end
+
+post '/memos' do
+  @memo = Memo.new(params['title'], params['detail'])
+  if @memo.save
+    redirect to('/memos')
+  else
+    @errors = @memo.errors
+    status 422
+    erb :new
+  end
+end
+
+get '/memos/:id' do |id|
+  @title = 'Show memo'
+  @memo = Memo.find(id)
+  if @memo.nil?
+    status 404
+    return erb :not_found
+  end
+  erb :show
+end
+
+get '/memos/:id/edit' do |id|
+  @title = 'Edit memo'
+  @memo = Memo.find(id)
+  if @memo.nil?
+    status 404
+    return erb :not_found
+  end
+  erb :edit
+end
+
+patch '/memos/:id' do |id|
+  @memo = Memo.find(id)
+  if @memo.nil?
+    status 404
+    return erb :not_found
+  end
+  @memo.title = params['title']
+  @memo.detail = params['detail']
+  if @memo.save
+    redirect to('/memos')
+  else
+    @errors = @memo.errors
+    status 422
+    erb :edit
+  end
+end
+
+delete '/memos/:id' do |id|
+  memo = Memo.find(id)
+  if memo.nil?
+    status 404
+    return erb :not_found
+  end
+  memo.destroy
+  redirect to('/memos')
+end
+
+not_found do
+  erb :not_found
+end
+
+helpers do
+  def h(value)
+    Rack::Utils.escape_html(value)
+  end
+end
---
+++ models/memo.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+class Memo
+  FILE_NAME = 'data/memos.csv'
+
+  attr_reader :id, :errors
+  attr_accessor :title, :detail
+
+  def initialize(title = nil, detail = nil, id = nil)
+    @id = id
+    @title = title
+    @detail = detail
+    @errors = []
+  end
+
+  def save
+    if @title.empty?
+      @errors << 'タイトルは必須です'
+      return false
+    end
+    if @id.nil?
+      @id = self.class.generate_id
+      create
+    else
+      update
+    end
+    true
+  end
+
+  def destroy
+    table = self.class.load_table
+    CSV.open(FILE_NAME, 'w') do |csv|
+      csv << table.headers
+      table.each do |row|
+        csv << row unless row['id'] == @id
+      end
+    end
+  end
+
+  def self.all
+    table = load_table
+    table.map do |row|
+      Memo.new(row['title'], row['detail'], row['id'])
+    end
+  end
+
+  def self.find(id)
+    all.find { |memo| memo.id == id }
+  end
+
+  def self.load_table
+    CSV.read(FILE_NAME, headers: true)
+  end
+
+  def self.generate_id
+    table = load_table
+    return 1 if table.empty?
+
+    max_id = table.map { |row| row['id'].to_i }.max
+    max_id + 1
+  end
+
+  private
+
+  def to_row
+    [@id, @title, @detail]
+  end
+
+  def create
+    CSV.open(FILE_NAME, 'a') do |csv|
+      csv << to_row
+    end
+  end
+
+  def update
+    table = self.class.load_table
+    CSV.open(FILE_NAME, 'w') do |csv|
+      csv << table.headers
+      table.each do |row|
+        row = to_row if row['id'] == @id
+        csv << row
+      end
+    end
+  end
+end
